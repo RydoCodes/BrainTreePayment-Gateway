4646d504d6d3691bde5a2136405b14c9605d6d1b
Vaibhav <vaibhav.maurya@centricconsulting.com>
2021-10-13 23:58:30 +0530

    Add project files.

---
+++ LibraryApp/Controllers/CheckoutController.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Braintree;
+using LibraryApp.Data.Services;
+using LibraryApp.Data.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryApp.Controllers
+{
+    public class CheckoutController : Controller
+    {
+        private readonly IBookService _bookService;
+        private readonly IBraintreeService _braintreeService;
+
+        public CheckoutController(IBookService courseService, IBraintreeService braintreeService)
+        {
+            _bookService = courseService;
+            _braintreeService = braintreeService;
+        }
+
+        public IActionResult Purchase(Guid id)
+        {
+
+            var book = _bookService.GetById(id);
+            if (book == null) return NotFound();
+
+            var gateway = _braintreeService.GetGateway();
+            var clientToken = gateway.ClientToken.Generate();
+            ViewBag.ClientToken = clientToken;
+
+            var data = new BookPurchaseVM
+            {
+                Id = book.Id,
+                Description = book.Description, 
+                Author = book.Author,
+                Thumbnail = book.Thumbnail,
+                Title = book.Title,
+                Price = book.Price,
+                Nonce = ""
+            };
+
+            return View(data);
+        }
+
+        public IActionResult Create(BookPurchaseVM model)
+        {
+            var gateway = _braintreeService.GetGateway();
+            var book = _bookService.GetById(model.Id);
+
+            var request = new TransactionRequest
+            {
+                Amount = Convert.ToDecimal(book.Price),
+                PaymentMethodNonce = model.Nonce,
+                Options = new TransactionOptionsRequest
+                {
+                    SubmitForSettlement = true
+                }
+            };
+
+            var result = gateway.Transaction.Sale(request);
+
+            if(result.IsSuccess())
+            {
+                return View("Success");
+            }
+            else
+            {
+                return View("Failure");
+            }
+        }
+
+        public IActionResult BrainTreePlans()
+        {
+            var gateway = _braintreeService.GetGateway();
+            var plans = gateway.Plan.All();
+
+            return View(plans);
+        }
+
+        public IActionResult SubscribeToPlan(string id)
+        {
+            var gateway = _braintreeService.GetGateway();
+
+            var subscriptionRequest = new SubscriptionRequest
+            {
+                PaymentMethodToken= "3mmmm8b",
+                PlanId=id
+            };
+
+            Result<Subscription> result = gateway.Subscription.Create(subscriptionRequest);
+
+            if (result.IsSuccess())
+            {
+                return View("Subscribed");
+            }
+            else
+            {
+                return View("NotSubscribed");
+            }
+        }
+    }
+}
---
+++ LibraryApp/Data/Services/BraintreeService.cs
@@ -0,0 +1,36 @@
+﻿using Braintree;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibraryApp.Data.Services
+{
+    public class BraintreeService : IBraintreeService
+    {
+        private readonly IConfiguration _config;
+        public BraintreeService(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public IBraintreeGateway CreateGateway()
+        {
+            var newGateway = new BraintreeGateway
+            {
+                Environment = Braintree.Environment.SANDBOX,
+                MerchantId = "yvgkhn9x4ss9t3qj",
+                PublicKey = "pcg3f64tx6bwbsd8",
+                PrivateKey = "734b12367b7c4b94ace00161f783022f"
+            };
+
+            return newGateway;
+        }
+
+        public IBraintreeGateway GetGateway()
+        {
+            return CreateGateway();
+        }
+    }
+}
